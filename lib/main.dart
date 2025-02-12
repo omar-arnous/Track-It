81dedad495bcd243d400fb6e56ce69a523851eca
omar <omar99.arnous@gmail.com>
2025-02-12 22:44:42 +0300

    configure firebase and link it to the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'package:trackit/config/injection_container.dart' as di;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await di.init();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
