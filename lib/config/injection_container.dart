34699d0fc7e3c87f3248c6ec8ad2da230bcc7954
omar <omar99.arnous@gmail.com>
2025-02-12 22:34:33 +0300

    Initializing the app

---
+++ lib/config/injection_container.dart
@@ -0,0 +1,15 @@
+import 'package:get_it/get_it.dart';
+
+final sl = GetIt.instance;
+
+Future<void> init() async {
+  // Bloc
+
+  // usecases
+
+  // repositories
+
+  // datasources
+
+  // external
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import 'package:trackit/config/injection_container.dart' as di;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Track It',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Center(
+        child: Text('TrackIt'),
+      ),
+    );
+  }
+}
