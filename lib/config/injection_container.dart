7a69e920586ca84bede92c08c9892a2eb4582fe8
omar <omar99.arnous@gmail.com>
2025-02-15 08:53:01 +0300

    inject the category functionality into injection container and consume the bloc

--- lib/config/injection_container.dart
+++ lib/config/injection_container.dart
@@ -5,13 +5,19 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:trackit/config/local_service.dart';
 import 'package:trackit/data/datasources/auth_cache_datasource.dart';
 import 'package:trackit/data/datasources/auth_remote_datasource.dart';
+import 'package:trackit/data/datasources/category_local_datasource.dart';
 import 'package:trackit/data/repositories/auth_repository_impl.dart';
+import 'package:trackit/data/repositories/category_repository_impl.dart';
 import 'package:trackit/domain/repositories/auth_repository.dart';
+import 'package:trackit/domain/repositories/category_repository.dart';
+import 'package:trackit/domain/usecases/category/add_category.dart';
+import 'package:trackit/domain/usecases/category/get_categories.dart';
 import 'package:trackit/domain/usecases/user/create_user.dart';
 import 'package:trackit/domain/usecases/user/login.dart';
 import 'package:trackit/domain/usecases/user/logout.dart';
 import 'package:trackit/domain/usecases/user/reset_password.dart';
 import 'package:trackit/presentation/blocs/auth/auth_bloc.dart';
+import 'package:trackit/presentation/blocs/category/category_bloc.dart';
 
 final sl = GetIt.instance;
 
@@ -26,15 +32,24 @@ Future<void> init() async {
     ),
   );
 
+  sl.registerFactory(
+    () => CategoryBloc(addCategories: sl(), getCategories: sl()),
+  );
+
   // usecases
   sl.registerLazySingleton(() => LoginUsecase(repository: sl()));
   sl.registerLazySingleton(() => LogoutUsecase(repository: sl()));
   sl.registerLazySingleton(() => CreateUserUsecase(repository: sl()));
   sl.registerLazySingleton(() => ResetPasswordUsecase(repository: sl()));
+  sl.registerLazySingleton(() => AddCategoryUsecase(repository: sl()));
+  sl.registerLazySingleton(() => GetCategoriesUsecase(repository: sl()));
 
   // repositories
   sl.registerLazySingleton<AuthRepository>(
     () => AuthRepositoryImpl(remoteDatasource: sl(), cacheDatasource: sl()),
+  );
+  sl.registerLazySingleton<CategoryRepository>(
+    () => CategoryRepositoryImpl(localDatasource: sl()),
   );
 
   // datasources
@@ -43,6 +58,9 @@ Future<void> init() async {
   );
   sl.registerLazySingleton<AuthCacheDatasource>(
     () => AuthCacheDatasourceImpl(sharedPreferences: sl()),
+  );
+  sl.registerLazySingleton<CategoryLocalDatasource>(
+    () => CategoryLocalDatasourceImpl(dbService: sl()),
   );
 
   // external
--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:trackit/core/theme/theme.dart';
 import 'package:trackit/presentation/blocs/auth/auth_bloc.dart';
 import 'package:trackit/config/router.dart';
+import 'package:trackit/presentation/blocs/category/category_bloc.dart';
 import 'firebase_options.dart';
 
 import 'package:trackit/config/injection_container.dart' as di;
@@ -30,7 +31,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     final routes = Routes();
     return MultiBlocProvider(
-      providers: [BlocProvider(create: (_) => di.sl<AuthBloc>())],
+      providers: [
+        BlocProvider(create: (_) => di.sl<AuthBloc>()),
+        BlocProvider(
+          create: (_) => di.sl<CategoryBloc>()..add(GetCategoriesEvent()),
+        ),
+      ],
       child: MaterialApp.router(
         title: 'Track It',
         theme: TrackItTheme.light(),
