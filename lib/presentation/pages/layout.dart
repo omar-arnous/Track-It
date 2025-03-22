b1909a5f4de1416c97d8121a22ca65c8386fbe95
omar <omar99.arnous@gmail.com>
2025-03-22 13:21:35 +0300

    Refactor: add fontawesome icons for the nav bar

--- lib/presentation/pages/layout.dart
+++ lib/presentation/pages/layout.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:go_router/go_router.dart';
 import 'package:trackit/core/constants/routes.dart';
 import 'package:trackit/presentation/blocs/app/app_bloc.dart';
@@ -49,19 +50,35 @@ class Layout extends StatelessWidget {
                   },
                   items: const [
                     BottomNavigationBarItem(
-                      icon: Icon(Icons.home_outlined),
+                      // icon: Icon(Icons.home_outlined),
+                      icon: FaIcon(
+                        FontAwesomeIcons.house,
+                        size: 20.0,
+                      ),
                       label: 'Home',
                     ),
                     BottomNavigationBarItem(
-                      icon: Icon(Icons.account_balance_outlined),
+                      // icon: Icon(Icons.account_balance_outlined),
+                      icon: FaIcon(
+                        FontAwesomeIcons.buildingColumns,
+                        size: 20.0,
+                      ),
                       label: 'Accounts',
                     ),
                     BottomNavigationBarItem(
-                      icon: Icon(Icons.layers),
+                      // icon: Icon(Icons.layers),
+                      icon: FaIcon(
+                        FontAwesomeIcons.layerGroup,
+                        size: 20.0,
+                      ),
                       label: 'Categories',
                     ),
                     BottomNavigationBarItem(
-                      icon: Icon(Icons.settings),
+                      // icon: Icon(Icons.settings),
+                      icon: FaIcon(
+                        FontAwesomeIcons.gear,
+                        size: 20.0,
+                      ),
                       label: 'Settings',
                     ),
                   ],
